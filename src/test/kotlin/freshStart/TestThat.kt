1739c90f2c8e2b05ce8696fc73173e2642785a02
guillaume SE <guillaume.saintetienne@sundayapp.com>
2022-02-02 00:21:02 +0100

    redémarrer le kata

--- src/test/kotlin/freshStart/TestThat.kt
+++ src/test/kotlin/freshStart/TestThat.kt
@@ -6,23 +6,7 @@ import io.kotest.matchers.shouldBe
 class TestThat : StringSpec({
 
     "Should return the fist letter " {
-        val actual = Accum.calculate("Z")
+        val actual = "z"
         actual shouldBe "Z"
     }
-
-    "Should return the fist letter uppercase" {
-        val actual = Accum.calculate("z")
-        actual shouldBe "Z"
-    }
-
-    "Should return the fist letter uppercase for a string a 2 letters" {
-        val actual = Accum.calculate("za")
-        actual shouldBe "Z-Aa"
-    }
-
-    "Should return the fist letter uppercase for a string a 3 letters" {
-        val actual = Accum.calculate("zaQ")
-        actual shouldBe "Z-Aa-Qqq"
-    }
-
 })
